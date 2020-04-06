d6b395b6f89c5c9775fc4f3129f59f8c3de26822
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2020-04-06 16:38:06 +0200

    Add testutils to help with checks and panics

---
+++ testutils/checks.go
@@ -0,0 +1,62 @@
+package testutils
+
+import (
+	"reflect"
+	"testing"
+)
+
+// CheckEqual checks if two values are deeply equal and calls t.Fatalf if not
+func CheckEqual(expected interface{}, got interface{}, t *testing.T) {
+	t.Helper()
+	if !reflect.DeepEqual(expected, got) {
+		t.Fatalf("Expected: %v, got %v", expected, got)
+	}
+}
+
+// CheckNil checks if value is nil
+func CheckNil(got interface{}, t *testing.T) {
+	t.Helper()
+	if !reflect.ValueOf(got).IsNil() {
+		t.Fatalf("Expected: nil, got %v", got)
+	}
+}
+
+// CheckNotNil checks if value is not nil
+func CheckNotNil(got interface{}, t *testing.T) {
+	t.Helper()
+	if reflect.ValueOf(got).IsNil() {
+		t.Fatalf("Expected: not nil, got nil")
+	}
+}
+
+// CheckError checks if there is an error
+func CheckError(got error, t *testing.T) {
+	t.Helper()
+	if got == nil {
+		t.Fatalf("Expected: error, got %v", got)
+	}
+}
+
+// CheckNotError checks if error value is not nil
+func CheckNotError(got error, t *testing.T) {
+	t.Helper()
+	if got != nil {
+		t.Fatalf("Expected: no error, got %v", got)
+	}
+}
+
+// CheckTrue checks if value is true
+func CheckTrue(got bool, t *testing.T) {
+	t.Helper()
+	if !got {
+		t.Fatalf("Expected: true, got %v", got)
+	}
+}
+
+// CheckFalse checks if value is false
+func CheckFalse(got bool, t *testing.T) {
+	t.Helper()
+	if got {
+		t.Fatalf("Expected: false, got %v", got)
+	}
+}
---
+++ testutils/panics.go
@@ -0,0 +1,21 @@
+package testutils
+
+import "testing"
+
+// ShouldNotPanic is used to assert that a function does not panic
+// Usage: defer testutils.ShouldNotPanic(t) at the point where the rest is expected to not panic
+func ShouldNotPanic(t *testing.T) {
+	t.Helper()
+	if r := recover(); r != nil {
+		t.Errorf("Unexpected panic")
+	}
+}
+
+// ShouldPanic is used to assert that a function does panic
+// Usage: defer testutils.ShouldNotPanic(t) at the point where the rest is expected to panic
+func ShouldPanic(t *testing.T) {
+	t.Helper()
+	if r := recover(); r == nil {
+		t.Errorf("Expected panic but got none")
+	}
+}
